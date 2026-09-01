e00be727c1c3be8a79ffb84bda70b87c873c6ae3
joserdz10 <56899615+joserdz10@users.noreply.github.com>
2026-08-31 19:26:27 -0600

    Delete independiente_openai_telegram_direct/src directory

--- independiente_openai_telegram_direct/src/setup-webhook.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const url = `${process.env.PUBLIC_BASE_URL.replace(/\/$/,"")}/telegram-webhook`;
-const r = await fetch(`https://api.telegram.org/bot${token}/setWebhook`, {
-  method:"POST", headers:{"content-type":"application/json"},
-  body:JSON.stringify({ url, secret_token: process.env.TELEGRAM_WEBHOOK_SECRET, allowed_updates:["message","callback_query"] })
-});
-console.log(await r.json());
